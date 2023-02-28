5ea452d4e18fee1571c5608e670db2c444f9dd93
Aluno <lucaskawakami5@gmail.com>
2023-02-28 10:35:48 -0300

    aaaa

---
+++ resources/views/empresa.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>empresas</h1>
+    <a href='/contato'>contato</a>
+    <a href='/'>home</a>
+    <a href='/produtos'>produtos</a>
+</body>
+</html>l
